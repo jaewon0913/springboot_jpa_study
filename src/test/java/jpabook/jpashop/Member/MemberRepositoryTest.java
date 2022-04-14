94db6a4a456da390921d60032433f31197d60153
jaewon0913 <jaewon9937@gmail.com>
2022-04-14 18:14:56 +0900

    [기능추가] 도메인 엔티티 추가 - 도메인 엔티티 클래스 추가(회원, 상품, 주문)

--- src/test/java/jpabook/jpashop/Member/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/Member/MemberRepositoryTest.java
@@ -2,7 +2,7 @@ package jpabook.jpashop.Member;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
