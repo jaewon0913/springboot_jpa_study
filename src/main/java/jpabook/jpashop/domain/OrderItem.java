5c9d2fd1f7477549567dae6a70c325a46734fabe
jaewon <jaewon9937@gmail.com>
2022-04-18 18:18:31 +0900

    [기능추가] 주문 도메인 개발 - 주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,5 +25,40 @@ public class OrderItem {
     private Order order;
 
     private int orderPrice; // 주문 가격
-    private int count; // 주문 수
+    private int count; // 주문 수량
+
+    //  ==  생성 메서드
+    /**
+     * 주문 상품 생성
+     * @param item
+     * @param orderPrice
+     * @param count
+     * @return
+     */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //  ==  비즈니스 로직 ==
+    /**
+     * 주문 취소
+     */
+    public void cancle() {
+        getItem().addStock(count);
+    }
+    
+    //  ==  조회 로직
+    /**
+     * 주문상품 전체 가격 조회
+     * @return
+     */
+    public int getTotalPrice(){
+        return getOrderPrice() * getCount();
+    }
 }
